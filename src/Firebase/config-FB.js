d2c8accc5f0801b3593d0bbfcaf3c8a60bbf5a6b
7SilviaT <eagles8485@gmail.com>
2022-05-30 12:19:21 -0500

    Installing FB

---
+++ src/Firebase/config-FB.js
@@ -0,0 +1,9 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDT--aaj5nFYZbV39QMqQ4MUHA9Fz7Ta8M",
+    authDomain: "burger-queen-6971c.firebaseapp.com",
+    projectId: "burger-queen-6971c",
+    storageBucket: "burger-queen-6971c.appspot.com",
+    messagingSenderId: "598340725993",
+    appId: "1:598340725993:web:1bd56bc9fcb31f475a5478"
+  };
