46995e64224db7be6786f51a7136d702b6643502
unknown <silviatrujillanop@gmail.com>
2022-05-30 16:29:50 -0500

    Fixing FB install

--- src/Firebase/config-FB.js
+++ src/Firebase/config-FB.js
@@ -1,3 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyDT--aaj5nFYZbV39QMqQ4MUHA9Fz7Ta8M",
@@ -7,4 +8,6 @@
     messagingSenderId: "598340725993",
     appId: "1:598340725993:web:1bd56bc9fcb31f475a5478"
   };
+
+  const app = initializeApp(firebaseConfig);
 
