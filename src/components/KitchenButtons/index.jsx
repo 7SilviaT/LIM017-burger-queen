8cc304c377a754b73621432c98d0adc852f3139d
unknown <silviatrujillanop@gmail.com>
2022-07-07 22:31:46 -0500

    Fixing password validation

--- src/components/KitchenButtons/index.jsx
+++ src/components/KitchenButtons/index.jsx
@@ -3,35 +3,21 @@ import style from "./kitchenbtns.module.css";
 
 const KitchenButtons = ({ setBtnSelect }) => {
   return (
-<<<<<<< HEAD
-    <div className={style.btnContainer}>
-      <button
-        className={style.btnWhite}
-=======
     <div>
       <button
         className={style.btnRed}
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
         onClick={() => setBtnSelect("pedidos en mesa")}
       >
         pedidos en mesa
       </button>
       <button
-<<<<<<< HEAD
-        className={style.btnOrange}
-=======
         className={style.btnBlue}
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
         onClick={() => setBtnSelect("en preparacion")}
       >
         en preparacion
       </button>
       <button
-<<<<<<< HEAD
-        className={style.btnGreen}
-=======
         className={style.btnYellow}
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
         onClick={() => setBtnSelect("despacho")}
       >
         despacho
@@ -39,8 +25,4 @@ const KitchenButtons = ({ setBtnSelect }) => {
     </div>
   );
 };
-<<<<<<< HEAD
 export default KitchenButtons;
-=======
-export default KitchenButtons;
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
 import KitchenButtons from "../../components/KitchenButtons";
-<<<<<<< HEAD
-import "../Kitchen/style.css";
-=======
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
 import { db } from "../../firebase";
 
 const Kitchen = () => {
@@ -43,11 +39,7 @@ const Kitchen = () => {
     setFilterOrder(filterOrders);
   };
 
-<<<<<<< HEAD
-  const updateDateFirestore = async (order, textStatus) => {
-=======
   const updateDateFirstore = async (order, textStatus) => {
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
     const orderUpdate = { ...order };
     orderUpdate.status = textStatus;
     delete orderUpdate.orderId;
@@ -62,21 +54,12 @@ const Kitchen = () => {
   }
   return (
     <div className="container">
-<<<<<<< HEAD
-      <div className="order-container" >        
-        <KitchenButtons setBtnSelect={updateFilter} />
-        {filterOrder.map((post) => (
-          <div key={post.orderId} className="order-container_client" >
-            <p>Cliente:{post.client}</p>
-            <p>Mesa:{post.table}</p>
-=======
       <div className="orders-container">
         <h1>pedidos:</h1>
         <KitchenButtons setBtnSelect={updateFilter} />
         {filterOrder.map((post) => (
           <div key={post.orderId}>
             <p>{post.client}</p>
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
             <ul>
               {post.orders.map((item) => (
                 <li key={item.orderId}>
@@ -90,23 +73,14 @@ const Kitchen = () => {
               ))}
             </ul>
             {post.status === "pedidos en mesa" && (
-<<<<<<< HEAD
-              <button className="orderButton"
-                onClick={() => updateDateFirestore(post, "en preparacion")}
-=======
               <button
                 onClick={() => updateDateFirstore(post, "en preparacion")}
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
               >
                 preparar
               </button>
             )}
             {post.status === "en preparacion" && (
-<<<<<<< HEAD
-              <button className="orderButton" onClick={() => updateDateFirestore(post, "despacho")}>
-=======
               <button onClick={() => updateDateFirstore(post, "despacho")}>
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
                 completado
               </button>
             )}
@@ -125,8 +99,4 @@ const Kitchen = () => {
   );
 };
 
-<<<<<<< HEAD
 export default Kitchen;
-=======
-export default Kitchen;
->>>>>>> dffa4de98696c54bc62466b71a1609ff19e5a0cb
